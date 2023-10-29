9be5fe4ef635a67cfc90e2c7ca3ab83d8cd14b26
Jayed Talukder <jayed.tz@gmail.com>
2023-10-29 10:23:39 +0100

    This route in this case won't need 'end'

--- src/Components/MainNavigation.js
+++ src/Components/MainNavigation.js
@@ -14,7 +14,6 @@ const MainNavigation = () => {
                     <NavLink
                         className={({isActive}) => isActive ? 'active' : undefined }
                         to='task-list'
-                        end
                     >Task List</NavLink>
                 </li>
             </ul>
